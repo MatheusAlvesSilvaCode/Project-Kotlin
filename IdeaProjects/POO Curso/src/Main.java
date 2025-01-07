f9f56c5720d1509df58bc901981d1445f024eb1e
MatheusAlvesSilvaCode <matheusalvesdev@outlook.com>
2025-01-07 11:30:32 +0000

    Aula POO encapsulamento

---
+++ IdeaProjects/POO Curso/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        ControleRemoto C = new ControleRemoto();
+        C.ligar();
+        C.abrirMenu();
+        C.ligarMudo();
+        C.abrirMenu();
+        C.abrirMenu();
+        C.abrirMenu();
+    }
+}
